052cae09ba5d6e1f2a5ccb8b003015a61295d9dd
Sarim95khan <sarim95khan@gmail.com>
2023-03-28 16:47:43 +0500

    first Commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,13 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/apple/Desktop/MyCode/Portfolio/my-portfolio/src/app/page": [
+      "/Users/apple/Desktop/MyCode/Portfolio/my-portfolio/src/app/globals.css"
+    ]
+  },
+  "/Users/apple/Desktop/MyCode/Portfolio/my-portfolio/src/app/page.tsx": [
+    "/Users/apple/Desktop/MyCode/Portfolio/my-portfolio/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ],
+  "/Users/apple/Desktop/MyCode/Portfolio/my-portfolio/src/app/layout.tsx": [
+    "/Users/apple/Desktop/MyCode/Portfolio/my-portfolio/src/app/globals.css"
+  ]
+}
